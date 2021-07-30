6829e99a351221bd1b250db6447e9ac3e1745819
Kudzai Chasinda <kudzai.chasinda@gmail.com>
2021-07-30 13:33:52 +0200

    add some basic theme properties to project

--- app/src/main/java/com/chase/kudzie/chasemusic/ui/theme/Theme.kt
+++ app/src/main/java/com/chase/kudzie/chasemusic/ui/theme/Theme.kt
@@ -1,2 +1,17 @@
 package com.chase.kudzie.chasemusic.ui.theme
 
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.runtime.Composable
+
+@Composable
+fun GalliumTheme(content: @Composable() () -> Unit) {
+
+    MaterialTheme(
+        colors = colors,
+        typography = typography,
+        shapes = shapes,
+        content = content
+    )
+}
--- app/src/main/java/com/chase/kudzie/chasemusic/ui/theme/Typography.kt
+++ app/src/main/java/com/chase/kudzie/chasemusic/ui/theme/Typography.kt
@@ -1,2 +1,43 @@
 package com.chase.kudzie.chasemusic.ui.theme
 
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val typography = Typography(
+    h1 = TextStyle(
+        fontSize = 28.sp,
+        fontWeight = FontWeight.Light,
+        letterSpacing = 1.15.sp,
+    ),
+    h2 = TextStyle(
+        fontSize = 15.sp,
+        letterSpacing = 1.15.sp,
+        fontWeight = FontWeight.Normal
+        // Add CAPS
+    ),
+    h3 = TextStyle(
+        fontSize = 14.sp,
+        letterSpacing = 0.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    body1 = TextStyle(
+        fontSize = 14.sp,
+        letterSpacing = 0.sp,
+        fontWeight = FontWeight.Normal
+    ),
+    button = TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Bold,
+        letterSpacing = 1.15.sp,
+        // Todo Add caps
+    ),
+    caption = TextStyle(
+        fontSize = 12.sp,
+        letterSpacing = 1.15.sp,
+        fontWeight = FontWeight.Normal
+        // Add caps
+    )
+)
